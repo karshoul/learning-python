b2309a669ab3eeaa4271744bd0b4a655e66996bc
Karshoul <Khuongmingo@gmail.com>
2023-10-01 18:10:02 +0700

    fix bug

--- 8/main.py
+++ 8/main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
 namSinh = int (input("Mời bạn nhập vào năm sinh: "))
-namHienTai = datetime().year
+namHienTai = datetime.now().year
 tuoi = namHienTai - namSinh
 print(f"Tuổi của bạn là: {tuoi}")
