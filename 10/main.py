6da1cd85c33fa17989b941c59f7139544833dae0
Karshoul <Khuongmingo@gmail.com>
2023-10-01 17:50:45 +0700

    #10 done

---
+++ 10/main.py
@@ -0,0 +1,28 @@
+thang = int (input("Mời bạn nhập vào tháng: "))
+if (thang < 1 or thang > 12):
+       print("Số nguyên không được nhỏ hơn 1 và lớn hơn 12 ")
+       exit()
+if (thang == 1):
+    print("Tháng một")
+if (thang == 2):
+        print("Tháng hai")
+if (thang == 3):
+        print("Tháng ba")
+if (thang == 4):
+       print("Tháng bốn")
+if (thang == 5):
+       print("Tháng năm")
+if (thang == 6):
+        print("Tháng sáu")
+if (thang == 7):
+        print("Tháng bảy")
+if (thang == 8):
+        print("Tháng tám")
+if (thang == 9):
+        print("Tháng chín")
+if (thang == 10):
+        print("Tháng mười")
+if (thang == 11):
+       print("Tháng mười một")
+if (thang == 12):
+       print("Tháng mười hai")
