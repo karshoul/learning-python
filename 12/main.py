2c7437990272118a6895c59aa1899e3f63aacec8
Karshoul <Khuongmingo@gmail.com>
2023-10-02 21:00:23 +0700

    fix code

--- 12/main.py
+++ 12/main.py
@@ -1,9 +1,9 @@
 numBer = int (input("Mời bạn nhập một số: "))
 i = 0
 
+print(f"Các số lẻ của dãy số từ 1 đến {numBer} là: ")
 while(i <= numBer):
         i += 1
         if(i % 2 ==0 ):
-          
-          print(f"Các số chẵn từ 1 đến {numBer} là: ",i)
+          print(i)
 
