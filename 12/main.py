0325e243defd95d171d17b06928c50967c18deca
Karshoul <Khuongmingo@gmail.com>
2023-10-02 20:24:46 +0700

    #12 done

---
+++ 12/main.py
@@ -0,0 +1,9 @@
+numBer = int (input("Mời bạn nhập một số: "))
+i = 0
+
+while(i <= numBer):
+        i += 1
+        if(i % 2 ==0 ):
+          
+          print(f"Các số chẵn từ 1 đến {numBer} là: ",i)
+
