52786b205da65a6082e0187a3af68d63583bd762
Karshoul <Khuongmingo@gmail.com>
2023-10-07 09:47:25 +0700

    #15 done

--- 15/main.py
+++ 15/main.py
@@ -1,7 +1,5 @@
-str = (input("Mời bạn nhập vào một chuỗi: "))
-
-new_str_list = list(reversed(str))
-new_str =  "".join(new_str_list)
-
+str = input("Mời bạn nhập vào một chuỗi: ")
+new_str_list = reversed(str)
+new_str = "".join(new_str_list)
 print(new_str)
 
