dbd7ebea91b61e235e0e6d14ba4ad38891957205
Karshoul <Khuongmingo@gmail.com>
2023-10-10 21:07:24 +0700

    add comment

--- 21/main.py
+++ 21/main.py
@@ -7,6 +7,6 @@ for kyTu in chuoi:
     # và ký tự đó xuất hiện nhiều hơn một lần trong chuỗi 
 
     if chuoi.count(kyTu) > 1 and kyTu not in kyTuTrungNhau_array:
-        kyTuTrungNhau_array.append(kyTu)
+        kyTuTrungNhau_array.append(kyTu) # # Nếu thỏa điều kiện, thêm ký tự vào danh sách ký tự trùng nhau
     
 print("Các ký tự trùng nhau trong chuỗi: ", kyTuTrungNhau_array)
